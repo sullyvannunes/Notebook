3228e41efedcfa9c637f284f06c4d1c4cf860fc5
Sullyvan Nunes <sullyvan.nunes@yahoo.com.br>
2017-10-04 23:43:58 -0300

    learning rspec

---
+++ spec/controllers/addresses_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe AddressesController, type: :controller do
+  let(:address) { Address.new }
+
+  it "Should has a instance of Address" do
+    expect(address).to be_instance_of(Address)
+    puts 'exemplo passou porra'
+  end
+end
